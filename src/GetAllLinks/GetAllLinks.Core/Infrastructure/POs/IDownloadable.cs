63eab84f02966931db716d314005e94f377226ef
spanishprisoner <jacek.mowinski@gmail.com>
2017-05-16 17:31:34 +0200

    Writing files

--- src/GetAllLinks/GetAllLinks.Core/Infrastructure/POs/IDownloadable.cs
+++ src/GetAllLinks/GetAllLinks.Core/Infrastructure/POs/IDownloadable.cs
@@ -3,6 +3,7 @@
 	public interface IDownloadable
 	{
 		string Url { get; set; }
+		string Name { get; set; }
 		void UpdateProgress(double completion, int speed);
 	}
 }
--- src/GetAllLinks/GetAllLinks.Droid/Services/Implementations/Downloader.cs
+++ src/GetAllLinks/GetAllLinks.Droid/Services/Implementations/Downloader.cs
@@ -1,8 +1,15 @@
 using System;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
+using Android;
+using Android.Content.PM;
+using Android.Support.V4.App;
+using Android.Support.V4.Content;
 using GetAllLinks.Core.Infrastructure.POs;
 using GetAllLinks.Core.Infrastructure.Services;
+using MvvmCross.Platform;
+using MvvmCross.Platform.Droid.Platform;
 
 namespace GetAllLinks.Droid.Services.Implementations
 {
@@ -13,36 +20,55 @@ namespace GetAllLinks.Droid.Services.Implementations
 
 		public async Task Download(IDownloadable downloadable)
 		{
+			GetPermission(Manifest.Permission.ReadExternalStorage);
+			GetPermission(Manifest.Permission.WriteExternalStorage);
+			var moviesDirectory = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryMovies).AbsolutePath;
+
 			var receivedBytes = 0;
 			var client = new WebClient();
 
-			using (var stream = await client.OpenReadTaskAsync(downloadable.Url))
+			using (var netStream = await client.OpenReadTaskAsync(downloadable.Url))
 			{
-				var buffer = new byte[ChunkSize];
-				var totalBytes = int.Parse(client.ResponseHeaders[HttpResponseHeader.ContentLength]);
-
-				var speed = 0;
-				var lastUpdate = DateTime.Now;
-				var lastReceivedBytes = 0;
-				for (;;)
+				using (var fileStream = File.Create($"{moviesDirectory}/{downloadable.Name}"))
 				{
-					var bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-					if (bytesRead == 0)
+					var buffer = new byte[ChunkSize];
+					var totalBytes = int.Parse(client.ResponseHeaders[HttpResponseHeader.ContentLength]);
+
+					var speed = 0;
+					var lastUpdate = DateTime.Now;
+					var lastReceivedBytes = 0;
+					for (;;)
 					{
-						await Task.Yield();
-						break;
+						var bytesRead = await netStream.ReadAsync(buffer, 0, buffer.Length);
+						if (bytesRead == 0)
+						{
+							await Task.Yield();
+							break;
+						}
+						receivedBytes += bytesRead;
+						await fileStream.WriteAsync(buffer, 0, buffer.Length);
+
+						if (DateTime.Now > lastUpdate + TimeSpan.FromMilliseconds(MeasureSpan))
+						{
+							speed = (receivedBytes - lastReceivedBytes) / MeasureSpan;
+							downloadable.UpdateProgress((double)receivedBytes / totalBytes, speed);
+							lastUpdate = DateTime.Now;
+							lastReceivedBytes = receivedBytes;
+						}
 					}
-					receivedBytes += bytesRead;
-					
-					if (DateTime.Now > lastUpdate + TimeSpan.FromMilliseconds(MeasureSpan))
-					{
-						speed = (receivedBytes - lastReceivedBytes) / MeasureSpan;
-						downloadable.UpdateProgress((double)receivedBytes / totalBytes, speed);
-						lastUpdate = DateTime.Now;
-						lastReceivedBytes = receivedBytes;
-					}
+					downloadable.UpdateProgress((double)receivedBytes / totalBytes, speed);
 				}
-				downloadable.UpdateProgress((double)receivedBytes / totalBytes, speed);
+			}
+		}
+
+		private void GetPermission(string requestedPermission)
+		{
+			var activity = Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity;
+			if (ContextCompat.CheckSelfPermission(activity,
+				requestedPermission) != Permission.Granted)
+			{
+				ActivityCompat.RequestPermissions(activity,
+				new[] { requestedPermission }, 0);
 			}
 		}
 	}
